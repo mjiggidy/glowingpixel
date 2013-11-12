4302027cc592896108097aa7d1194890274fe3e3
mjiggidy <michael@glowingpixel.com>
2013-11-12 04:03:53 +0000

    Initial commit. Splash page with no real functionality.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="Description" content="Michael Jordan is a cinematographer based in Los Angeles.">
+	<link href="http://fonts.googleapis.com/css?family=Vollkorn:400italic" rel="stylesheet" type="text/css" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		
+			$(".initial").hide();
+			$("#band").fadeIn(1000, function(){$("#backdrop").fadeIn(2000);});
+		
+		});
+	</script>
+	<title>Michael Jordan</title>
+</head>
+<body>
+
+	<div id="backdrop" class="initial"></div>
+
+	<section id="band" class="initial">
+		<header>
+			<h1>Michael Jordan &middot; Cinematographer</h1>
+			<a title="Contact Me" href="mailto:michael@glowingpixel.com">michael@glowingpixel.com</a>
+		</header>
+	</section>
+
+<?php /*	<video src="video/unl.mp4" controls></video> */ ?>
+
+</body>
+</html>
